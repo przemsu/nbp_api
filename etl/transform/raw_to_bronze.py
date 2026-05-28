85b40c18be75a2d78dd2564dd77fba0e27bc8829
Przemyslaw Suchan <przemek.suchan@gmail.com>
2026-05-28 11:58:10 +0200

    Bronze layer in duckdb

---
+++ etl/transform/raw_to_bronze.py
@@ -0,0 +1,47 @@
+from src.utils.db_creator import create_db, insert_raw_data
+from src.utils.load_config import get_curr_code
+from pathlib import Path
+import json
+import logging
+import duckdb
+
+
+logger = logging.getLogger(__name__)
+
+def insert_into_bronze_layer():
+
+    config = get_curr_code()
+    db_path = config['paths']['db_path']
+
+    create_db()
+
+    con = duckdb.connect(db_path)
+
+    p = Path(config['paths']['raw_data_dir'])
+    files = list(p.rglob('*.json'))
+    logger.info(f"Found {len(files)} files in {p}")
+
+    success_count = 0
+    error_count = 0
+   
+    for f in files:
+        try:
+            # Wyciąganie metadanych ze ścieżki
+            extraction_date = f"{f.parts[2].split('=')[-1]}-{f.parts[3].split('=')[-1]}-{f.parts[4].split('=')[-1]}"
+            endpoint_type = f.stem
+            
+            # Wczytywanie treści
+            with open(f, 'r', encoding='utf-8') as file:
+                json_content = json.load(file)
+
+            # Ładowanie do bazy
+            insert_raw_data(con, endpoint_type, extraction_date, json_content)
+            success_count += 1
+            
+        except Exception as e:
+            logger.error(f"Failed to process file {f}: {e}")
+            error_count += 1
+            continue
+
+    con.close()
+    logger.info(f"Bronze layer load finished. Success: {success_count}, Errors: {error_count}")
--- src/utils/db_creator.py
+++ src/utils/db_creator.py
@@ -1,22 +1,36 @@
 import duckdb
 from src.utils.load_config import get_curr_code
+import logging
+
+# Create logger dedicated for this module
+logger = logging.getLogger(__name__)
+
+config = get_curr_code()
 
 def create_db():
 
-    config = get_curr_code()
     db_create_path = config['paths']['db_path']
 
     con = duckdb.connect(db_create_path)
 
     con.execute('''
-        CREATE TABLE IF NOT EXISTS nbp_api_data_daily_load (
+        CREATE OR REPLACE TABLE raw_nbp_api_data (
         endpoint_type VARCHAR,
         extraction_date DATE,
-        loaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         raw_content JSON,
+        loaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         UNIQUE(endpoint_type, extraction_date)
         )
     '''
     )
 
     con.close()
+
+def insert_raw_data(con, endpoint_type, extraction_date, file_path):
+
+    con.execute("""
+                INSERT INTO raw_nbp_api_data (endpoint_type, extraction_date, raw_content)
+                VALUES (?, ?, ?) 
+                ON CONFLICT DO NOTHING;"""
+              , [endpoint_type, extraction_date, file_path]
+    )
