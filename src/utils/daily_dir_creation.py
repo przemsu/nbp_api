c0d4bae2cfce9b2974e97f4d4d3b1e875a65cb9c
Przemyslaw Suchan <przemek.suchan@gmail.com>
2026-06-04 21:26:20 +0200

    Modifying script for backload if missing data

--- src/utils/daily_dir_creation.py
+++ src/utils/daily_dir_creation.py
@@ -1,15 +1,20 @@
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime, date
 
-def create_dir_daily_load(dir_path) -> Path:
+def create_dir_daily_load(raw_dir_path, load_date: date = None) -> Path:
 
-    now = datetime.now()
+    if not load_date:
+        load_date = datetime.now().date()
 
+    year = load_date.year 
+    month = f'{load_date.month:02d}'
+    day = f'{load_date.day:02d}'
+    
     path = (
-        Path(dir_path)
-        / f"year={now:%Y}"
-        / f"month={now:%m}"
-        / f"day={now:%d}"
+        Path(raw_dir_path)
+        / f"year={year}"
+        / f"month={month}"
+        / f"day={day}"
     )
 
     path.mkdir(parents=True, exist_ok=True)
